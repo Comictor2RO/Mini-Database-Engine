a8014005c0249b0786fda00cb9706a916fb08afb
Comictor2RO <comictor2ro@gmail.com>
2026-04-16 14:26:43 +0200

    Network Local Server open and accept clients finished

--- Networking/NetworkServer.cpp
+++ Networking/NetworkServer.cpp
@@ -10,14 +10,21 @@ void NetworkServer::openServer()
     acceptor.open(tcp::v4());
     acceptor.bind(tcp::endpoint(tcp::v4(), port));
     acceptor.listen();
+    std::cout << "Server started at 127.0.0.1:" << port << std::endl;
 }
 
 // Accepts incoming connections and handles them async
 void NetworkServer::acceptConnections()
 {
     acceptor.async_accept(io_context, [this](asio::error_code ec, tcp::socket socket) {
-        if (!ec)
+        if (!ec) {
             handleClient(std::move(socket));
+            std::cout << "Client connected" << std::endl;
+        }
+        else {
+            std::cerr << "Error accepting connection: " << ec.message() << std::endl;
+        }
+
         acceptConnections();
     });
 }
